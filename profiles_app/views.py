5493bc1c8deadb41e1d2628bcc71aea9ff79cc5a
Amjed Saleel K <amjedsaleel@gmail.com>
2021-01-15 18:16:04 +0530

    add user profile search

--- profiles_app/views.py
+++ profiles_app/views.py
@@ -3,6 +3,7 @@ from rest_framework.response import Response
 from rest_framework import status
 from rest_framework import viewsets
 from rest_framework.authentication import TokenAuthentication
+from rest_framework import filters
 
 from . import serializers
 from . models import UserProfile
@@ -111,3 +112,6 @@ class UserProfileViewSet(viewsets.ModelViewSet):
     queryset = UserProfile.objects.all()
     authentication_classes = (TokenAuthentication, )
     permission_classes = (permissions.UpdateOwnProfile, )
+    filter_backends = (filters.SearchFilter, )
+    search_fields = ('name', 'email')
+
